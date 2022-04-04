10596e676b2513c29becb5438d3544df0e48429a
Andreas Kaeberlein <andreas.kaeberlein@siemens.com>
2022-04-04 10:59:06 +0200

    bugfix: write mask calculation

--- src/pcimem_main.c
+++ src/pcimem_main.c
@@ -204,7 +204,7 @@ int main(int argc, char **argv)
                 if ( (uint32_t) ~0 != uint32MskVal ) {
                     uint32Temp = (uint32_t) *((volatile uint32_t *) pcimem_ptr(&pciHandle));    // read
                     uint32Temp = uint32Temp & ((uint32_t) ~uint32MskVal);                       // clear all masked bits
-                    uint32Temp = uint32Temp | uint32WriteVal;                                   // or set bits into again
+                    uint32Temp = uint32Temp | (uint32WriteVal & uint32MskVal);                  // or set bits into again
                 }
                 *((volatile uint32_t *) pcimem_ptr(&pciHandle)) = (uint32_t) uint32Temp;        // write value
                 /* check */
